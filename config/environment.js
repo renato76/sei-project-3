eab4c1b02e02f78e7dc2c5301678b52344322e7b
renato76 <renmignogna@gmail.com>
2020-11-10 12:06:31 +0000

    added

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/scavhunt'
-const port = 4000
+const port = process.env.PORT || 4000
 const secret = process.env.SECRET || 'shhhh its a secret'
 
 module.exports = { dbURI, port, secret }
